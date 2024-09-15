2c4ede12f265a9b14113550cec9ff191b610c150
EnxhiHoxhaj <hoxhajenxhi1@gmail.com>
2024-09-16 00:48:21 +0200

    fix timetable file. End

--- resources/views/timetable.blade.php
+++ resources/views/timetable.blade.php
@@ -20,12 +20,13 @@
           <h6 class="card-subtitle mb-2 text-body-secondary">Compagnia ferroviaria: {{$train->company}}</h6>
           <h6 class="card-subtitle mb-2 text-body-secondary">Treno numero: {{$train->train_code}}</h6>
           <h6 class="card-subtitle mb-2 text-body-secondary">Numero di vagoni: {{$train->number_of_carriages}}</h6>
-          @if ($train->on_time = 0 )
-          <h6 class="card-subtitle mb-2 text-body-secondary">Ritardo: {{$train->on_time}}</h6>
+          @if ($train->on_time = 1 )
+          <h6 class="card-subtitle mb-2 text-body-secondary">Ritardo: Treno in orario</h6>
           @else
+          <h6 class="card-subtitle mb-2 text-body-secondary">Ritardo: Treno in Ritardo</h6>
           @endif
           @if ($train->cancelled = 0 )
-          <h6 class="card-subtitle mb-2 text-body-secondary">Cancellato: {{$train->cancelled}}</h6>
+          <h6 class="card-subtitle mb-2 text-body-secondary">Cancellato</h6>
           @else
           @endif
         </div>
